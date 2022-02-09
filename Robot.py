c2fde243bf5d080220d01ae668ec0f4f755f1dc6
Robbie <roby.1383@yahoo.com>
2022-02-09 14:46:45 +0100

    Added movement - need to fix same velocity movement

--- Robot.py
+++ Robot.py
@@ -1,32 +1,76 @@
 import random
+import math
+import numpy as np
 
 class Robot():
 
     def __init__(self, WIDTH, HEIGHT, size):
         self.WIDTH = WIDTH
         self.HEIGHT = HEIGHT
-        self.size = size
+        self.radius = int (size/2)
 
-        self.x = random.randint(4 + size, WIDTH - size - 4)
-        self.y = random.randint(4 + size, HEIGHT - size - 4)
+        self.x = random.randint(4 + self.radius, WIDTH - self.radius - 4)
+        self.y = random.randint(4 + self.radius, HEIGHT - self.radius - 4)
+
+        self.frontX = self.x + self.radius
+        self.frontY = self.y
 
         self.Vl = 0
         self.Vr = 0
         self.theta = 0
+        self.speed = 0.5
 
-    def move(self, movement):
+    def move(self, movement): #[W, S, O, L, X, T, G]
         if self.hitWall():
             print("hit wall")
-            return
 
         if movement[0] == 1:
-            self.Vl += 1
-            self.theta += 1
-            self.x += 1
+            self.Vl += self.speed
+        if movement[1] == 1:
+            self.Vl -= self.speed
+        if movement[2] == 1:
+            self.Vr += self.speed
+        if movement[3] == 1:
+            self.Vr -= self.speed
+        if movement[4] == 1:
+            self.Vl = 0
+            self.Vr = 0
+        if movement[5] == 1:
+            self.Vl += self.speed
+            self.Vr += self.speed
+        if movement[6] == 1:
+            self.Vl -= self.speed
+            self.Vr -= self.speed
 
-        self.theta = self.theta % 361
-        return self.theta
+        if self.Vr != 0 or self.Vl != 0:
+            print("Vl ", self.Vl)
+            print("Vr ", self.Vr)
+            print()
+
+            if self.Vr == self.Vl:
+                R = 100
+                w = 0
+
+            else :
+                R = self.radius * (self.Vl + self.Vr)/(self.Vr - self.Vl)
+                w = (self.Vr - self.Vl)/(self.radius*2)
+                ICC = [self.x - R * np.sin(self.theta), self.y + R * np.cos(self.theta)]
+
+                result = np.transpose(np.matmul(
+                    np.array([[np.cos(w), -np.sin(w), 0],
+                             [np.sin(w), np.cos(w), 0],
+                            [0, 0, 1]]),
+                    np.transpose(np.array([self.x - ICC[0], self.y - ICC[1], self.theta]))) + np.array([ICC[0], ICC[1], w])).transpose()
+                self.x = result[0]
+                self.y = result[1]
+                self.theta = result[2]
+            self.rotate(self.theta)
+
+
+    def rotate(self, angle):
+        self.frontX = self.x + np.cos(angle) * self.radius
+        self.frontY = self.y + np.sin(angle) * self.radius
 
     def hitWall(self):
-        return self.x <= 4 + self.size or self.x >= self.WIDTH - self.size - 4 \
-               or self.y <= 4 + self.size or self.y >= self.HEIGHT - self.size - 4
+        return self.x <= 5 + self.radius or self.x >= self.WIDTH - self.radius - 5 \
+               or self.y <= 5 + self.radius or self.y >= self.HEIGHT - self.radius - 5
--- run.py
+++ run.py
@@ -23,13 +23,8 @@ class Simulation ():
     def __init__(self):
         pygame.init()
         self.robot = Robot(WIDTH, HEIGHT, 101)
-        self.robotImage = pygame.image.load(r'Robot.png')
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Modular Robot Simulator")
-        self.image = self.robotImage
-        self.rect = self.image.get_rect()
-        self.rect.center = (self.robot.x - 101, self.robot.y - 101)
-        self.angle = 0
 
         self.running = True
         self.clock = pygame.time.Clock()
@@ -50,13 +45,10 @@ class Simulation ():
         """
         self.screen.fill(WHITE)
         pygame.draw.rect(self.screen, DARKGRAY, (1, 1, WIDTH-2, HEIGHT-2), 5)
-
-
-    def rotateImage(self, angle):
-        car_img = pygame.image.load(r'Robot.png')
-        img = pygame.transform.rotate(car_img, angle)
-        self.screen.blit(img, (self.robot.x - 101, self.robot.y - 101))
+        pygame.draw.circle(self.screen, PURPLE, (self.robot.x, self.robot.y), 50)
+        pygame.draw.line(self.screen, BLACK, (self.robot.x, self.robot.y), (self.robot.frontX, self.robot.frontY), 1)
         pygame.display.flip()
+
 
     def update(self):
         """
@@ -68,10 +60,9 @@ class Simulation ():
                 pygame.quit()
 
         keys = pygame.key.get_pressed()
-        movement = [keys[pygame.K_w], keys[pygame.K_s], keys[pygame.K_o], keys[pygame.K_x], keys[pygame.K_t], keys[pygame.K_g]]
+        movement = [keys[pygame.K_w], keys[pygame.K_s], keys[pygame.K_o], keys[pygame.K_l], keys[pygame.K_x], keys[pygame.K_t], keys[pygame.K_g]]
 
-        newTheta = self.robot.move(movement)
-        self.rotateImage(newTheta)
+        self.robot.move(movement)
 
 
     def stop(self):
