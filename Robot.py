89bd60bfb1dcef9f36cfe243400e869c3da77366
Robbie <roby.1383@yahoo.com>
2022-02-09 17:27:59 +0100

    Finished movement

--- Robot.py
+++ Robot.py
@@ -1,5 +1,4 @@
 import random
-import math
 import numpy as np
 
 class Robot():
@@ -7,7 +6,7 @@ class Robot():
     def __init__(self, WIDTH, HEIGHT, size):
         self.WIDTH = WIDTH
         self.HEIGHT = HEIGHT
-        self.radius = int (size/2)
+        self.radius = int(size/2)
 
         self.x = random.randint(4 + self.radius, WIDTH - self.radius - 4)
         self.y = random.randint(4 + self.radius, HEIGHT - self.radius - 4)
@@ -20,57 +19,59 @@ class Robot():
         self.theta = 0
         self.speed = 0.5
 
-    def move(self, movement): #[W, S, O, L, X, T, G]
+    def move(self, movement):
         if self.hitWall():
+            # Add collision check
             print("hit wall")
 
-        if movement[0] == 1:
+        # Check keys for movement
+        # movement = [w, s, o, l, x, t, g]
+
+        if movement[0] == 1 or movement[5] == 1:
             self.Vl += self.speed
-        if movement[1] == 1:
+        if movement[1] == 1 or movement[6] == 1:
             self.Vl -= self.speed
-        if movement[2] == 1:
+        if movement[2] == 1 or movement[5] == 1:
             self.Vr += self.speed
-        if movement[3] == 1:
+        if movement[3] == 1 or movement[6] == 1:
             self.Vr -= self.speed
         if movement[4] == 1:
             self.Vl = 0
             self.Vr = 0
-        if movement[5] == 1:
-            self.Vl += self.speed
-            self.Vr += self.speed
-        if movement[6] == 1:
-            self.Vl -= self.speed
-            self.Vr -= self.speed
 
+        # If it's moving
         if self.Vr != 0 or self.Vl != 0:
-            print("Vl ", self.Vl)
-            print("Vr ", self.Vr)
-            print()
 
+            # Make sure not to get a division by zero when velocities are the same
             if self.Vr == self.Vl:
-                R = 100
+                R = 10000
                 w = 0
-
-            else :
+            else:
                 R = self.radius * (self.Vl + self.Vr)/(self.Vr - self.Vl)
                 w = (self.Vr - self.Vl)/(self.radius*2)
-                ICC = [self.x - R * np.sin(self.theta), self.y + R * np.cos(self.theta)]
 
-                result = np.transpose(np.matmul(
-                    np.array([[np.cos(w), -np.sin(w), 0],
-                             [np.sin(w), np.cos(w), 0],
-                            [0, 0, 1]]),
-                    np.transpose(np.array([self.x - ICC[0], self.y - ICC[1], self.theta]))) + np.array([ICC[0], ICC[1], w])).transpose()
-                self.x = result[0]
-                self.y = result[1]
-                self.theta = result[2]
+            # Compute ICC
+            ICC = [self.x - R * np.sin(self.theta), self.y + R * np.cos(self.theta)]
+            result = np.transpose(np.matmul(
+                np.array([[np.cos(w), -np.sin(w), 0],
+                         [np.sin(w), np.cos(w), 0],
+                        [0, 0, 1]]),
+                np.transpose(np.array([self.x - ICC[0], self.y - ICC[1], self.theta]))) + np.array([ICC[0], ICC[1], w])).transpose()
+
+            # Transfer results from the ICC computation
+            self.x = result[0]
+            self.y = result[1]
+            self.theta = result[2]
             self.rotate(self.theta)
 
+        return self.Vl, self.Vr, np.round(np.degrees(self.theta) % 360, 2)
 
     def rotate(self, angle):
+        # Rotate the robot at a certain angle from the x-axis
         self.frontX = self.x + np.cos(angle) * self.radius
         self.frontY = self.y + np.sin(angle) * self.radius
 
     def hitWall(self):
+        # Check if the robot is hitting the wall
         return self.x <= 5 + self.radius or self.x >= self.WIDTH - self.radius - 5 \
                or self.y <= 5 + self.radius or self.y >= self.HEIGHT - self.radius - 5
--- run.py
+++ run.py
@@ -8,67 +8,81 @@ HEIGHT = 1000
 # A list of RGB values for the colours used in the game.
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-LIGHTGRAY = (216, 220, 227)
 DARKGRAY = (125, 130, 138)
-GREEN = (120, 161, 88)
-RED = (196, 96, 96)
-BLUE = (86, 152, 209)
-DARKBLUE = (52, 84, 112)
-LIGHTBLUE = (129, 174, 214)
-YELLOW = (245, 192, 47)
 PURPLE = (114, 85, 163)
+LIGHTPURPLE = (185, 167, 217)
 
-class Simulation ():
+def getColour(pressed):
+    if pressed:
+        return PURPLE
+    else:
+        return LIGHTPURPLE
+
+class Simulation():
 
     def __init__(self):
         pygame.init()
         self.robot = Robot(WIDTH, HEIGHT, 101)
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pygame.display.set_mode((WIDTH + 350, HEIGHT))
         pygame.display.set_caption("Modular Robot Simulator")
+        self.font = pygame.font.SysFont("Pokemon GB.ttf", 50)
 
         self.running = True
         self.clock = pygame.time.Clock()
 
     def run(self):
-        """
-        Runs the thread for the graphics.
-        """
         while self.running:
             self.clock.tick(60)
-            self.show()
-            self.update()
+            keys, velocities = self.update()
+            self.show(keys, velocities)
 
-
-    def show(self):
-        """
-        Fills the screen with the required graphics based on the map and the agents.
-        """
+    def show(self, keys, velocities):
         self.screen.fill(WHITE)
+
+        # Fill in robot environment
         pygame.draw.rect(self.screen, DARKGRAY, (1, 1, WIDTH-2, HEIGHT-2), 5)
         pygame.draw.circle(self.screen, PURPLE, (self.robot.x, self.robot.y), 50)
         pygame.draw.line(self.screen, BLACK, (self.robot.x, self.robot.y), (self.robot.frontX, self.robot.frontY), 1)
+
+        # Fill in keys
+        pygame.draw.rect(self.screen, getColour(keys[0]), pygame.Rect(WIDTH + 50, HEIGHT / 2 - 100, 50, 50))
+        self.screen.blit(self.font.render("w", 105, WHITE), (WIDTH + 60, HEIGHT / 2 - 95))
+        pygame.draw.rect(self.screen, getColour(keys[1]), pygame.Rect(WIDTH + 50, HEIGHT / 2 , 50, 50))
+        self.screen.blit(self.font.render("s", 105, WHITE), (WIDTH + 65, HEIGHT / 2 + 5))
+        pygame.draw.rect(self.screen, getColour(keys[5]), pygame.Rect(WIDTH + 150, HEIGHT / 2 - 100, 50, 50))
+        self.screen.blit(self.font.render("t", 105, WHITE), (WIDTH + 170, HEIGHT / 2 - 90))
+        pygame.draw.rect(self.screen, getColour(keys[6]), pygame.Rect(WIDTH + 150, HEIGHT / 2 , 50, 50))
+        self.screen.blit(self.font.render("g", 105, WHITE), (WIDTH + 165, HEIGHT / 2 + 5))
+        pygame.draw.rect(self.screen, getColour(keys[2]), pygame.Rect(WIDTH + 250, HEIGHT / 2 - 100, 50, 50))
+        self.screen.blit(self.font.render("o", 105, WHITE), (WIDTH + 265, HEIGHT / 2 - 95))
+        pygame.draw.rect(self.screen, getColour(keys[3]), pygame.Rect(WIDTH + 250, HEIGHT / 2 , 50, 50))
+        self.screen.blit(self.font.render("l", 105, WHITE), (WIDTH + 270, HEIGHT / 2 + 10))
+        pygame.draw.rect(self.screen, getColour(keys[4]), pygame.Rect(WIDTH + 150, HEIGHT / 2 + 100, 50, 50))
+        self.screen.blit(self.font.render("x", 105, WHITE), (WIDTH + 165, HEIGHT / 2 + 105))
+
+        # Display velocities
+        left = "V left = " + str(velocities[0])
+        self.screen.blit(self.font.render(left, 105, BLACK), (WIDTH + 50, HEIGHT / 2 - 305))
+        right = "V right = " + str(velocities[1])
+        self.screen.blit(self.font.render(right, 105, BLACK), (WIDTH + 50, HEIGHT / 2 - 255))
+        theta = "θ = " + str(velocities[2]) + "°"
+        self.screen.blit(self.font.render(theta, 105, BLACK), (WIDTH + 50, HEIGHT / 2 - 205))
         pygame.display.flip()
 
-
     def update(self):
-        """
-        Updates the parameters for the game.
-        """
+        # Quit the simulation
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.stop()
                 pygame.quit()
 
+        # Get pressed keys and update robot position
         keys = pygame.key.get_pressed()
         movement = [keys[pygame.K_w], keys[pygame.K_s], keys[pygame.K_o], keys[pygame.K_l], keys[pygame.K_x], keys[pygame.K_t], keys[pygame.K_g]]
-
-        self.robot.move(movement)
-
+        velocities = self.robot.move(movement)
+        return movement, velocities
 
     def stop(self):
-        """
-        Stops the thread.
-        """
         self.running = False
         exit()
 
